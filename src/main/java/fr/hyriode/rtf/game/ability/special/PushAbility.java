50741d7d3fb93d421e3d734e383ed9e20574795c
Caliix <calyxadresse@gmail.com>
2023-04-29 14:30:25 +0200

    Add friendly fire when its enabled on push ability and nerf it a bit
    Took 9 minutes

--- src/main/java/fr/hyriode/rtf/game/ability/special/PushAbility.java
+++ src/main/java/fr/hyriode/rtf/game/ability/special/PushAbility.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PushAbility extends RTFAbility {
 
@@ -19,13 +20,16 @@ public class PushAbility extends RTFAbility {
 
     @Override
     public void use(Player player) {
-        for (Entity entity : player.getNearbyEntities(5, 5, 5)
+        Stream<Entity> playerStream = player.getNearbyEntities(3, 3, 3)
                 .stream()
                 .filter(entity -> entity instanceof Player)
-                .filter(entity -> HyriRTF.get().getGame().getPlayer(entity.getUniqueId()) != null)
-                .filter(entity -> !HyriRTF.get().getGame().getPlayer(entity.getUniqueId()).getTeam().contains(player.getUniqueId()))
-                .collect(Collectors.toList())) {
+                .filter(entity -> HyriRTF.get().getGame().getPlayer(entity.getUniqueId()) != null);
 
+        if (!HyriRTF.get().getGame().getPlayer(player.getUniqueId()).getTeam().isFriendlyFire()) {
+            playerStream = playerStream.filter(entity -> !HyriRTF.get().getGame().getPlayer(entity.getUniqueId()).getTeam().contains(player.getUniqueId()));
+        }
+
+        for (Entity entity : playerStream.collect(Collectors.toList())) {
             final Vector push = player.getLocation().subtract(entity.getLocation()).toVector().normalize().multiply(-3);
 
             if (entity.getUniqueId() != player.getUniqueId()){
